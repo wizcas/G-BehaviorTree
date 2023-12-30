a2923d16c3439f81905327476e74af4a81fcde30
Wizcas Chen <chen@0x1c.dev>
2023-12-30 15:53:48 +0800

    fix save context tests

--- GBT.Sharp/GBT.Sharp.Core.Tests/SaveContextTests.cs
+++ GBT.Sharp/GBT.Sharp.Core.Tests/SaveContextTests.cs
@@ -13,8 +13,15 @@ public class SaveContextTests {
                 )
             );
 
+        tree.Runtime.RunningNode = tree.FindNodeByName("cb1");
+        BehaviorTree tree2 = new();
+        tree2.Load(tree.Save());
         var bin = tree.Runtime.Save();
-        var runtime2 = new TreeRuntime(tree);
+        var runtime2 = new TreeRuntime(tree2);
         runtime2.Load(bin);
+        Assert.Equivalent(tree.Runtime.RunningNode?.ID, runtime2.RunningNode?.ID);
+        Assert.Equivalent(tree.Runtime.NodeContexts.Keys, runtime2.NodeContexts.Keys);
+        Assert.Equal(tree.Runtime.NodeContexts.Values.Select(v => (v.Node.ID, v.State)),
+                     runtime2.NodeContexts.Values.Select(v => (v.Node.ID, v.State)));
     }
 }
